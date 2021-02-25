1918e6ab5342f8166d0302703db8daaae5fa5cfa
Brage Rosberg <brage70@gmail.com>
2021-02-25 22:04:01 +0100

    Add route switch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
+import React from 'react';
+import { HashRouter as Routes, Switch, Route, Link } from 'react-router-dom';
+import Home from './components/home/';
+import './index.css';
+
 const App = () => (
-  <div className="app">
-    <h2>Sku Bare</h2>
-    <p>Under utvikling</p>
-  </div>
+  <Routes basename="/">
+    <div>
+
+      <nav className="navbar__routes">
+
+        <ul>
+
+          <li><Link to="/">Hjem</Link></li>
+
+          <li><Link to="/meny">Meny</Link></li>
+
+          <li><Link to="/bilder">Bilder</Link></li>
+
+          <li><Link to="/corona">Corona</Link></li>
+
+          <li><Link to="/om-oss">Om oss</Link></li>
+
+        </ul>
+
+      </nav>
+
+      <Switch>
+
+        <Route path="/">
+          <Home />
+        </Route>
+
+      </Switch>
+
+    </div>
+  </Routes>
 );
 
 export default App;
---
+++ src/components/home/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Home = () => (
+  <div className="app">
+    <h2>Sku Bare</h2>
+    <p>Under utvikling</p>
+  </div>
+)
+
+export default Home;
