f5e9e0b5f223d403c82f7aed1e65093678aa891b
Brage Rosberg <brage70@gmail.com>
2021-03-01 21:27:18 +0100

    Add illustration for landingpage

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import illustration from '../../assets/bar-header.jpeg';
 import './home.css';
 
 const Home = () => (
   <div className="app">
     <h2>Sku Bare</h2>
     <h4>Bar - Scene - Trivsel</h4>
+    <img src={illustration} alt="skubba" className="app__illustration" />
     <p>Under utvikling</p>
   </div>
 )
